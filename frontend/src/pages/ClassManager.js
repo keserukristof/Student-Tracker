f32710d21544bf95f22241b1b8c19ba2e7b178ad
Kristof Keseru <Kristof_Keseru@epam.com>
2022-07-13 18:44:01 +0200

    Created class manager page

--- frontend/src/pages/ClassManager.js
+++ frontend/src/pages/ClassManager.js
@@ -1,7 +1,33 @@
+import React, { useEffect } from "react";
+
+import useStudentStore from '../store'
+
+import ClassCard from "../components/ClassCard";
+import { createClass } from "../helpers/classCreator";
+
 function ClassManager() {
+    const { students, getStudents } = useStudentStore(
+        (state) => ({
+            students: state.students,
+            getStudents: state.getStudents
+        })
+    )
+
+    useEffect(() => {
+        getStudents();
+    }, []);
+
+    const classesWithStudents = createClass(students);
+
     return (
-        <div>
-            <h2>Class Manager</h2>
+        <div class="container">
+            <div class="row">
+                {Object.keys(classesWithStudents).map(specificClass => {
+                    return (
+                        <ClassCard class={specificClass} studentsForClass={classesWithStudents[specificClass]} />
+                    );
+                })}
+            </div>
         </div>
     );
 }
