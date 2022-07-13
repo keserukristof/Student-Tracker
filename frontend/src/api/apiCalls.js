d5a3602910d1b1b47dc31256f3401ca6308d29e2
Kristof Keseru <Kristof_Keseru@epam.com>
2022-07-13 18:41:50 +0200

    Added save and delete to api calls

--- frontend/src/api/apiCalls.js
+++ frontend/src/api/apiCalls.js
@@ -6,3 +6,13 @@ export async function getStudents() {
     const res = await axios.get(`${LOCALHOST}/students`);
     return res.data;
 }
+
+export async function saveStudent(student) {
+    const res = await axios.post(`${LOCALHOST}/students`, student);
+    return res;
+}
+
+export async function deleteStudent(studentId) {
+    const res = await axios.delete(`${LOCALHOST}/students/${studentId}`);
+    return res;
+}
