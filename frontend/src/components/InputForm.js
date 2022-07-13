69fd88d13d5abddf991376b9d614e2fbb971ec3f
Kristof Keseru <Kristof_Keseru@epam.com>
2022-07-13 18:42:35 +0200

    Created input form

--- frontend/src/components/InputForm.js
+++ frontend/src/components/InputForm.js
@@ -1,34 +1,117 @@
-import React, { useState } from "react";
 import useStudentStore from '../store'
 
-function InputForm() {
-    const addStudent = useStudentStore((state) => state.addStudent);
+import React, { useState } from 'react';
+import { saveStudent } from '../api/apiCalls';
 
-    const handleStudentSubmit = () => {}
+function InputForm() {
+    const { students, addStudent } = useStudentStore(
+        (state) => ({
+            students: state.students,
+            addStudent: state.addStudent,
+        })
+    )
+
+    const [student, setStudent] = useState({});
+
+    function handleChange(e) {
+        const name = e.target.name;
+        const value = e.target.value;
+        setStudent(student => ({ ...student, [name]: value }))
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        const newStudent = {
+            id: students.length + 1,
+            firstName: student.firstName,
+            lastName: student.lastName,
+            email: student.email,
+            gender: student.gender,
+            classFirstPart: student.class.charAt(0),
+            classSecondPart: student.class.charAt(student.class.length - 1)
+        };
+        addStudent(newStudent);
+        saveStudent(newStudent);
+    }
 
     return (
-        <form class="container p-5">
-            <div class="mb-3">
-                <label for="firstName" class="form-label">First Name</label>
-                <input type="text" class="form-control" id="firstName" aria-describedby="emailHelp" required />
+        <form class="container p-5" onSubmit={handleSubmit}>
+            <div class="row">
+                <div class="mb-3 col">
+                    <label for="firstName" class="form-label">First Name</label>
+                    <input
+                        name='firstName'
+                        value={student.firstName}
+                        onChange={handleChange}
+                        type="text"
+                        id="firstName"
+                        class="form-control"
+                        required />
+                </div>
+                <div class="mb-3 col">
+                    <label for="lastName" class="form-label">Last Name</label>
+                    <input
+                        name='lastName'
+                        value={student.lastName}
+                        onChange={handleChange}
+                        type="text"
+                        class="form-control"
+                        id="lastName"
+                        required />
+                </div>
             </div>
-            <div class="mb-3">
-                <label for="lastName" class="form-label">Last Name</label>
-                <input type="text" class="form-control" id="lastName" aria-describedby="emailHelp" required />
+            <div class="row">
+                <div class="mb-3 col-8">
+                    <label for="class" class="form-label">Class</label>
+                    <input
+                        name='class'
+                        value={student.class}
+                        onChange={handleChange}
+                        type="text"
+                        class="form-control"
+                        id="class"
+                        required />
+                </div>
+                <div class='col-4 mt-4'>
+                    <div class="form-check">
+                        <input
+                            name="gender"
+                            value="Male"
+                            onClick={handleChange}
+                            class="form-check-input"
+                            type="radio"
+                            id="male" />
+                        <label class="form-check-label" for="male">
+                            Male
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input
+                            name="gender"
+                            value="Female"
+                            onClick={handleChange}
+                            class="form-check-input"
+                            type="radio"
+                            id="female" />
+                        <label class="form-check-label" for="female">
+                            Female
+                        </label>
+                    </div>
+                </div>
             </div>
-            <div class="mb-3">
-                <label for="class" class="form-label">Class</label>
-                <input type="text" class="form-control" id="class" required />
+            <div class="mb-3 col">
+                <label for="email" class="form-label">Email</label>
+                <input
+                    name='email'
+                    value={student.email}
+                    onChange={handleChange}
+                    type="text" class="form-control"
+                    id="email"
+                    required />
             </div>
-            <div class="btn-group mb-3" role="group" aria-label="Basic radio toggle button group">
-                <input type="radio" class="btn-check" name="btnradio" id="male" autocomplete="off" checked />
-                <label class="btn btn-outline-primary" for="male">Male</label>
-
-                <input type="radio" class="btn-check" name="btnradio" id="female" autocomplete="off" />
-                <label class="btn btn-outline-primary" for="female">Female</label>
-
+            <div class="row justify-content-center">
+                <button type="submit" class="btn btn-light col-5">Submit</button>
             </div>
-            <button type="submit" class="btn btn-light col-12">Submit</button>
         </form>
     );
 }
