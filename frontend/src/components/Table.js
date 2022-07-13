87ba4f7e6d3b571cabed210756722b549861d139
Kristof Keseru <Kristof_Keseru@epam.com>
2022-07-13 18:42:54 +0200

    Created dynamic table

--- frontend/src/components/Table.js
+++ frontend/src/components/Table.js
@@ -1,29 +1,52 @@
 import React, { useEffect } from "react";
+import { deleteStudent } from "../api/apiCalls";
 import useStudentStore from '../store'
 
-
 function Table() {
-    const { students, getStudents } = useStudentStore(
+    const { students, getStudents, sortStudents, removeStudent } = useStudentStore(
         (state) => ({
             students: state.students,
-            getStudents: state.getStudents
+            getStudents: state.getStudents,
+            sortStudents: state.sortStudents,
+            removeStudent: state.removeStudent
         })
     )
 
-    useEffect( () => {
+    useEffect(() => {
         getStudents();
-        console.log(students)
-     }, []);
+    }, []);
 
     return (
         <table class="table container p-5">
             <thead>
                 <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">First Name</th>
-                    <th scope="col">Last Name</th>
+                    <th onClick={() => {
+                        sortStudents("id")
+                    }} scope="col">
+                        ID
+                        <button>
+                            <ion-icon name="arrow-down-outline"></ion-icon>
+                        </button>
+                    </th>
+                    <th onClick={() => {
+                        sortStudents("firstName")
+                    }} scope="col">
+                        First Name
+                        <button>
+                            <ion-icon name="arrow-down-outline"></ion-icon>
+                        </button>
+                    </th>
+                    <th onClick={() => {
+                        sortStudents("lastName")
+                    }} scope="col">
+                        Last Name
+                        <button>
+                            <ion-icon name="arrow-down-outline"></ion-icon>
+                        </button>
+                    </th>
                     <th scope="col">Gender</th>
                     <th scope="col">Class</th>
+                    <th scope="col">Email</th>
                 </tr>
             </thead>
             <tbody>
@@ -32,8 +55,16 @@ function Table() {
                         <tr>
                             <th scope="row">{student.id}</th>
                             <td>{student.firstName}</td>
-                            <td>Thornton</td>
-                            <td>@fat</td>
+                            <td>{student.lastName}</td>
+                            <td>{student.gender}</td>
+                            <td>{student.classFirstPart}.{student.classSecondPart}</td>
+                            <td>{student.email}</td>
+                            <button onClick={() => {
+                                deleteStudent(student.id);
+                                removeStudent(student.id);
+                            }}>
+                                <ion-icon name="close-circle-outline"></ion-icon>
+                            </button>
                         </tr>
                     )
                 })}
